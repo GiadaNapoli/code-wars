1c8a19960bd6df4c06e43028bbb332025d4b5370
GiadaNapoli <giadanapoli94@gmail.com>
2024-05-05 20:35:18 +0200

    new codewars challenge sum it continuously

---
+++ 7kyu/sum-it-continuously.js
@@ -0,0 +1,19 @@
+// DESCRIPTION:
+// Make a function "add" that will be able to sum elements of list continuously and return a new list of sums.
+
+// For example:
+
+// add [1,2,3,4,5] == [1, 3, 6, 10, 15], because it's calculated like
+// this : [1, 1 + 2, 1 + 2 + 3, 1 + 2 + 3 + 4, 1 + 2 + 3 + 4 + 5]
+
+//My solution
+
+function add(arr) {
+	let sum = 0;
+	let result = [];
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+		result.push(sum);
+	}
+	return result;
+}
